d006e1e2714a0e9ad803886400d78b50a31a7a03
Paul DAO <dao.van.paul@gmail.com>
2024-03-28 14:44:01 -0400

    FEAT main observer element add and remove reveal class

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import './style.scss';
 import { sendEmail } from "./email.service.ts";
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    entry.isIntersecting ? entry.target.classList.add('reveal') : entry.target.classList.remove('reveal');
+
+  });
+});
+
+const projectDescriptionEls = document.querySelectorAll('.project__description');
+projectDescriptionEls.forEach(descriptionEl => observer.observe(descriptionEl));
+
+
 const contactFormEl: HTMLElement | null = document.getElementById('contact-form');
 
 contactFormEl!.addEventListener('submit', async (event: SubmitEvent) => {
